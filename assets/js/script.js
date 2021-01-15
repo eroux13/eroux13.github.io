d168649835513be07aed715526dadf037f1d691f
EJ <ej.roux13@gmail.com>
2021-01-15 02:29:47 -0800

    added comments for reference

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,18 @@
 var form = document.querySelector("form");
 form.addEventListener("submit", contact, false);
 function contact(event) {
+    // Prevent page from reloading after button click
     event.preventDefault();
 
     var target = event.target || event.srcElement;
     var message = "";
 
+    // Loop through all inupt fields
     for (var i = 0; i < target.length; ++i) {
         if (target[i].type != "text" && target[i].type != "textarea" && target[i].type != "email") {
             continue;
         }
+        // Add input name and value followed by a line break
         message += target[i].name + ": " + target[i].value + "\r\n";
     }
     target.elements["body"].value = message;
